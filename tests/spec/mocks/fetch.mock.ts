05ea18345cd514a3d7f2a1a59a7a8865dc9e5a6c
Omkar Desai <omkar.desai.1991@gmail.com>
2025-01-06 16:10:41 -0700

    Hotfixes

--- tests/spec/mocks/fetch.mock.ts
+++ tests/spec/mocks/fetch.mock.ts
@@ -1,16 +1,16 @@
 import personalData from '../../../public/data/personal.data.json'
 
-const mockFetch = (url: string) => {
+const mockFetch = (url: RequestInfo | URL, init?: RequestInit) => {
     switch (url) {
         case '/data/personal.data.json': {
-            return {
+            return Promise.resolve({
                 ok: true,
                 status: 200,
                 json: () => Promise.resolve(personalData),
-            }
+            } as Response)
         }
         default: {
-            throw new Error(`Unhandled request: ${url}`)
+            throw new Error('mockFetch::Unhandled request url - ' + (url as string))
         }
     }
 }
